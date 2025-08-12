71684b57fb6501cf13c95d76f923c9033d40881b
Alfonso Tamés <mufufu@Mufufu-MacBook-Pro.local>
2025-08-12 14:56:53 -0600

    Begin upload functionality

---
+++ src/main/java/com/materiam/controllers/FileUploadController.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.materiam.controllers;
+
+import org.primefaces.PrimeFaces;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.event.FilesUploadEvent;
+import org.primefaces.model.file.UploadedFile;
+import org.primefaces.model.file.UploadedFiles;
+import org.primefaces.util.EscapeUtils;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+/**
+ *
+ * @author mufufu
+ */
+@Named(value = "fileUploadController")
+@RequestScoped
+public class FileUploadController {
+    
+    private UploadedFile file;
+    private UploadedFiles files;
+    private String dropZoneText = "Drop zone p:inputTextarea demo.";
+    
+    
+    
+    public void upload() {
+        if (file != null) {
+            FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+
+    public void uploadMultiple() {
+        if (files != null) {
+            for (UploadedFile f : files.getFiles()) {
+                FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        }
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "*** UPLOADING ***");
+        FacesMessage message = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void handleFileUploadTextarea(FileUploadEvent event) {
+        String jsVal = "PF('textarea').jq.val";
+        String fileName = EscapeUtils.forJavaScript(event.getFile().getFileName());
+        PrimeFaces.current().executeScript(jsVal + "(" + jsVal + "() + '\\n\\n" + fileName + " uploaded.')");
+    }
+
+    public void handleFilesUpload(FilesUploadEvent event) {
+        for (UploadedFile f : event.getFiles().getFiles()) {
+            FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+    
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public UploadedFiles getFiles() {
+        return files;
+    }
+
+    public void setFiles(UploadedFiles files) {
+        this.files = files;
+    }
+    
+    public String getDropZoneText() {
+        return dropZoneText;
+    }
+
+    public void setDropZoneText(String dropZoneText) {
+        this.dropZoneText = dropZoneText;
+    }
+}

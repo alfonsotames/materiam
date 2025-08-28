48eb953419f5c7f2a3a14d0c996cc78128d04029
Alfonso Tamés <alfonso@tames.com>
2025-08-27 23:44:04 -0600

    Added upload file support

--- src/main/java/com/materiam/controllers/FileUploadController.java
+++ src/main/java/com/materiam/controllers/FileUploadController.java
@@ -15,6 +15,17 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,69 +38,43 @@ import java.util.logging.Logger;
 @Named(value = "fileUploadController")
 @RequestScoped
 public class FileUploadController {
-    
-    private UploadedFile file;
-    private UploadedFiles files;
-    private String dropZoneText = "Drop zone p:inputTextarea demo.";
-    
-    
-    
-    public void upload() {
-        if (file != null) {
-            FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
+        private String destination = "/tmp/alfonso/hola/";
+
+        
+
+    public void upload(FileUploadEvent event) {
+        FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        
+        UploadedFile file = event.getFile();
+        // Do what you want with the file
+        try {
+            copyFile(event.getFile().getFileName(), file.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
-    public void uploadMultiple() {
-        if (files != null) {
-            for (UploadedFile f : files.getFiles()) {
-                FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
-                FacesContext.getCurrentInstance().addMessage(null, message);
+    public void copyFile(String fileName, InputStream in) {
+        Path path = Paths.get(destination);
+        try {
+            
+            Files.createDirectories(path);
+            System.out.println("Directory created successfully at: " + path.toAbsolutePath());
+                    
+                    
+            // write the inputStream to a FileOutputStream
+            OutputStream out = new FileOutputStream(new File(destination + fileName));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            while ((read = in.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
             }
+            in.close();
+            out.flush();
+            out.close();
+            System.out.println("New file uploaded: " + (destination + fileName));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-    }
-
-    public void handleFileUpload(FileUploadEvent event) {
-        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "*** UPLOADING ***");
-        FacesMessage message = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
-        FacesContext.getCurrentInstance().addMessage(null, message);
-    }
-
-    public void handleFileUploadTextarea(FileUploadEvent event) {
-        String jsVal = "PF('textarea').jq.val";
-        String fileName = EscapeUtils.forJavaScript(event.getFile().getFileName());
-        PrimeFaces.current().executeScript(jsVal + "(" + jsVal + "() + '\\n\\n" + fileName + " uploaded.')");
-    }
-
-    public void handleFilesUpload(FilesUploadEvent event) {
-        for (UploadedFile f : event.getFiles().getFiles()) {
-            FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-        }
-    }
-    
-    public UploadedFile getFile() {
-        return file;
-    }
-
-    public void setFile(UploadedFile file) {
-        this.file = file;
-    }
-
-    public UploadedFiles getFiles() {
-        return files;
-    }
-
-    public void setFiles(UploadedFiles files) {
-        this.files = files;
-    }
-    
-    public String getDropZoneText() {
-        return dropZoneText;
-    }
-
-    public void setDropZoneText(String dropZoneText) {
-        this.dropZoneText = dropZoneText;
     }
 }
